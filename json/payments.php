be0e3212afa38d65aea3cf17f740c14bb5e75ae4
sandymabrouk <38265219+sandymabrouk@users.noreply.github.com>
2018-04-16 22:16:11 +0000

    Update payments.php

--- json/payments.php
+++ json/payments.php
@@ -1,24 +1,20 @@
 <?php
 //require("json.php");
+require("json.php");
 //displayTable("payments");
+displayTable("payments","order_id , code, amount");
 require("../db.php");
-$sql = "SELECT students.name
-FROM s students
-JOIN ss payments ON (students.name = payments.amount)";
-
-
+$sql = "SELECT students.name , payments.code , payments.amount
+FROM students
+JOIN payments ON (students.code = payments.code)";
 $result = mysqli_query($conn, $sql);
-
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
     while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["code"] ;
+        echo "id: " . $row["code"] . "name: " . $row["name"] ."payment amount: " . $row["amount"];
     }
 } else {
     echo "0 results";
 }
-
 mysqli_close($conn);
-
-
 ?>
